24b9c83d0a1c5f47ce90839509e84a9a1d90d899
David0 <81240773+David0J@users.noreply.github.com>
2026-03-13 13:25:43 +0200

    Update PlayerData.java

--- src/main/java/me/davidjawhar/oneblock/player/PlayerData.java
+++ src/main/java/me/davidjawhar/oneblock/player/PlayerData.java
@@ -3,31 +3,44 @@ package me.davidjawhar.oneblock.player;
 import java.util.UUID;
 
 public class PlayerData {
+
     private final UUID uuid;
     private String name;
+    private int blocksBroken;
     private int level;
-    private long blocksBroken;
-    private long dateCreated;
-    private long lastLogin;
 
-    public PlayerData(UUID uuid, String name) {
+    public PlayerData(UUID uuid, String name, int blocksBroken, int level) {
         this.uuid = uuid;
         this.name = name;
-        this.level = 0;
-        this.blocksBroken = 0;
-        this.dateCreated = System.currentTimeMillis();
-        this.lastLogin = System.currentTimeMillis();
+        this.blocksBroken = blocksBroken;
+        this.level = level;
     }
 
-    public UUID getUuid() { return uuid; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public int getLevel() { return level; }
-    public void setLevel(int level) { this.level = level; }
-    public long getBlocksBroken() { return blocksBroken; }
-    public void setBlocksBroken(long blocksBroken) { this.blocksBroken = blocksBroken; }
-    public long getDateCreated() { return dateCreated; }
-    public void setDateCreated(long dateCreated) { this.dateCreated = dateCreated; }
-    public long getLastLogin() { return lastLogin; }
-    public void setLastLogin(long lastLogin) { this.lastLogin = lastLogin; }
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBlocksBroken() {
+        return blocksBroken;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBlocksBroken(int blocksBroken) {
+        this.blocksBroken = blocksBroken;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
 }
