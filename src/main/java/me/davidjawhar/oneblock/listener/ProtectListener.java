bf4d6c0149ea15abe3ef90559ee1bd06c6e7e2e8
David0 <81240773+David0J@users.noreply.github.com>
2026-03-13 13:36:12 +0200

    Update ProtectListener.java

--- src/main/java/me/davidjawhar/oneblock/listener/ProtectListener.java
+++ src/main/java/me/davidjawhar/oneblock/listener/ProtectListener.java
@@ -2,70 +2,103 @@ package me.davidjawhar.oneblock.listener;
 
 import me.davidjawhar.oneblock.island.IslandData;
 import me.davidjawhar.oneblock.island.IslandManager;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockFromToEvent;
+import org.bukkit.event.block.BlockExplodeEvent;
 import org.bukkit.event.block.BlockPistonExtendEvent;
 import org.bukkit.event.block.BlockPistonRetractEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 
+import java.util.UUID;
+
 public class ProtectListener implements Listener {
+
     private final IslandManager islandManager;
 
-    public ProtectListener(me.davidjawhar.oneblock.OneBlockPlugin plugin, IslandManager islandManager) {
+    public ProtectListener(IslandManager islandManager) {
         this.islandManager = islandManager;
     }
 
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler
     public void onBreak(BlockBreakEvent event) {
         Block block = event.getBlock();
-        for (IslandData data : islandManager.getIslands().values()) {
-            if (block.getWorld().getName().equals(data.getWorldName())
-                    && block.getX() == data.getX()
-                    && block.getY() == data.getY() - 1
-                    && block.getZ() == data.getZ()) {
+        Player player = event.getPlayer();
+
+        IslandData island = islandManager.getIsland(player.getUniqueId());
+        if (island == null) return;
+
+        if (isCoreBlock(block, island)) {
+            if (block.getType() == Material.BEDROCK) {
                 event.setCancelled(true);
-                return;
             }
         }
-        if (islandManager.isOfficialOneBlock(block) && islandManager.getOwner(block) != null
-                && !event.getPlayer().getUniqueId().equals(islandManager.getOwner(block))
-                && event.getPlayer().hasPermission("oneblock.admin") == false) {
+    }
+
+    @EventHandler
+    public void onBlockExplode(BlockExplodeEvent event) {
+        event.blockList().removeIf(this::isAnyIslandCoreBlock);
+    }
+
+    @EventHandler
+    public void onEntityExplode(EntityExplodeEvent event) {
+        event.blockList().removeIf(this::isAnyIslandCoreBlock);
+    }
+
+    @EventHandler
+    public void onPistonExtend(BlockPistonExtendEvent event) {
+        if (event.getBlocks().stream().anyMatch(this::isAnyIslandCoreBlock)) {
             event.setCancelled(true);
         }
     }
 
-    @EventHandler(ignoreCancelled = true)
-    public void onExplosion(EntityExplodeEvent event) {
-        event.blockList().removeIf(this::isCoreProtected);
-    }
-
-    @EventHandler(ignoreCancelled = true)
-    public void onPistonExtend(BlockPistonExtendEvent event) {
-        if (event.getBlocks().stream().anyMatch(this::isCoreProtected)) event.setCancelled(true);
-    }
-
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler
     public void onPistonRetract(BlockPistonRetractEvent event) {
-        if (event.getBlocks().stream().anyMatch(this::isCoreProtected)) event.setCancelled(true);
-    }
-
-    @EventHandler(ignoreCancelled = true)
-    public void onLiquid(BlockFromToEvent event) {
-        if (isCoreProtected(event.getToBlock())) event.setCancelled(true);
-    }
-
-    private boolean isCoreProtected(Block block) {
-        if (islandManager.isOfficialOneBlock(block)) return true;
-        for (IslandData data : islandManager.getIslands().values()) {
-            if (block.getWorld().getName().equals(data.getWorldName())
-                    && block.getX() == data.getX()
-                    && block.getY() == data.getY() - 1
-                    && block.getZ() == data.getZ()) return block.getType() == Material.BEDROCK;
+        if (event.getBlocks().stream().anyMatch(this::isAnyIslandCoreBlock)) {
+            event.setCancelled(true);
         }
+    }
+
+    private boolean isCoreBlock(Block block, IslandData island) {
+        Location loc = block.getLocation();
+
+        // Main OneBlock
+        if (island.isOneBlock(loc)) {
+            return true;
+        }
+
+        // Bedrock under the OneBlock
+        return loc.getWorld() != null
+                && loc.getWorld().getName().equals(island.getWorldName())
+                && loc.getBlockX() == island.getX()
+                && loc.getBlockY() == island.getY() - 1
+                && loc.getBlockZ() == island.getZ();
+    }
+
+    private boolean isAnyIslandCoreBlock(Block block) {
+        Location loc = block.getLocation();
+
+        for (IslandData island : islandManager.getAllIslands().values()) {
+            if (loc.getWorld() == null) continue;
+            if (!loc.getWorld().getName().equals(island.getWorldName())) continue;
+
+            // OneBlock
+            if (island.isOneBlock(loc)) {
+                return true;
+            }
+
+            // Bedrock under it
+            if (loc.getBlockX() == island.getX()
+                    && loc.getBlockY() == island.getY() - 1
+                    && loc.getBlockZ() == island.getZ()) {
+                return true;
+            }
+        }
+
         return false;
     }
 }
