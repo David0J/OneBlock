c84625a475516f349f574bae2a40f0dab36ca78a
David0 <81240773+David0J@users.noreply.github.com>
2026-03-13 13:18:25 +0200

    Update JoinListener.java

--- src/main/java/me/davidjawhar/oneblock/listener/JoinListener.java
+++ src/main/java/me/davidjawhar/oneblock/listener/JoinListener.java
@@ -1,31 +1,66 @@
 package me.davidjawhar.oneblock.listener;
 
 import me.davidjawhar.oneblock.OneBlockPlugin;
+import me.davidjawhar.oneblock.island.IslandData;
 import me.davidjawhar.oneblock.island.IslandManager;
-import me.davidjawhar.oneblock.ui.BossBarManager;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 public class JoinListener implements Listener {
+
     private final OneBlockPlugin plugin;
     private final IslandManager islandManager;
-    private final BossBarManager bossBarManager;
 
-    public JoinListener(OneBlockPlugin plugin, IslandManager islandManager, BossBarManager bossBarManager) {
+    public JoinListener(OneBlockPlugin plugin, IslandManager islandManager) {
         this.plugin = plugin;
         this.islandManager = islandManager;
-        this.bossBarManager = bossBarManager;
     }
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
-        var player = event.getPlayer();
-        plugin.getPlayerDataManager().getOrCreate(player.getUniqueId(), player.getName());
-        islandManager.getOrCreateIsland(player);
-        if (plugin.getConfig().getBoolean("teleport-returning-on-join", true)) {
-            plugin.getServer().getScheduler().runTask(plugin, () -> islandManager.teleportHome(player));
+
+        Player player = event.getPlayer();
+
+        IslandData island = islandManager.getOrCreateIsland(player);
+
+        teleportToIsland(player, island);
+    }
+
+    @EventHandler
+    public void onRespawn(PlayerRespawnEvent event) {
+
+        Player player = event.getPlayer();
+
+        IslandData island = islandManager.getIsland(player.getUniqueId());
+
+        if (island != null) {
+            Location location = getIslandLocation(island);
+            event.setRespawnLocation(location);
         }
-        plugin.getServer().getScheduler().runTaskLater(plugin, () -> bossBarManager.show(player), 5L);
+    }
+
+    private void teleportToIsland(Player player, IslandData island) {
+
+        Location location = getIslandLocation(island);
+
+        Bukkit.getScheduler().runTask(plugin, () -> player.teleport(location));
+    }
+
+    private Location getIslandLocation(IslandData island) {
+
+        World world = Bukkit.getWorld(plugin.getConfig().getString("world-name", "oneblock_world"));
+
+        return new Location(
+                world,
+                island.getX() + 0.5,
+                island.getY() + 1,
+                island.getZ() + 0.5
+        );
     }
 }
