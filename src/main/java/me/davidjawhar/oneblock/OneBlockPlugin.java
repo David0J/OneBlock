9895c40f159ccff8d474e4b70e0a9952a2b98367
David0 <81240773+David0J@users.noreply.github.com>
2026-03-13 13:28:11 +0200

    Update OneBlockPlugin.java

--- src/main/java/me/davidjawhar/oneblock/OneBlockPlugin.java
+++ src/main/java/me/davidjawhar/oneblock/OneBlockPlugin.java
@@ -5,67 +5,84 @@ import me.davidjawhar.oneblock.island.IslandManager;
 import me.davidjawhar.oneblock.level.LevelManager;
 import me.davidjawhar.oneblock.listener.BlockBreakListener;
 import me.davidjawhar.oneblock.listener.JoinListener;
-import me.davidjawhar.oneblock.listener.ProtectListener;
 import me.davidjawhar.oneblock.player.PlayerDataManager;
 import me.davidjawhar.oneblock.ui.BossBarManager;
-import org.bukkit.command.PluginCommand;
+import me.davidjawhar.oneblock.world.VoidChunkGenerator;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.WorldCreator;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Objects;
-
 public class OneBlockPlugin extends JavaPlugin {
-    private PlayerDataManager playerDataManager;
+
     private IslandManager islandManager;
+    private PlayerDataManager playerDataManager;
     private LevelManager levelManager;
     private BossBarManager bossBarManager;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
-        saveResourceIfMissing("players.yml");
-        saveResourceIfMissing("islands.yml");
+
+        createVoidWorld();
 
         this.playerDataManager = new PlayerDataManager(this);
-        this.levelManager = new LevelManager(this);
-        this.islandManager = new IslandManager(this, playerDataManager, levelManager);
-        this.bossBarManager = new BossBarManager(this, playerDataManager, levelManager, islandManager);
+        this.islandManager = new IslandManager(this);
+        this.levelManager = new LevelManager(this, playerDataManager);
+        this.bossBarManager = new BossBarManager(this, playerDataManager, levelManager);
 
-        islandManager.ensureWorldExists();
+        getServer().getPluginManager().registerEvents(
+                new JoinListener(this, islandManager, bossBarManager), this
+        );
+        getServer().getPluginManager().registerEvents(
+                new BlockBreakListener(this, islandManager, playerDataManager, levelManager, bossBarManager), this
+        );
 
-        getServer().getPluginManager().registerEvents(new JoinListener(this, islandManager, bossBarManager), this);
-        getServer().getPluginManager().registerEvents(new BlockBreakListener(this, islandManager, playerDataManager, levelManager, bossBarManager), this);
-        getServer().getPluginManager().registerEvents(new ProtectListener(this, islandManager), this);
+        OBCommand obCommand = new OBCommand(this, islandManager, playerDataManager, levelManager, bossBarManager);
+        if (getCommand("ob") != null) {
+            getCommand("ob").setExecutor(obCommand);
+            getCommand("ob").setTabCompleter(obCommand);
+        }
 
-        PluginCommand cmd = Objects.requireNonNull(getCommand("ob"));
-        OBCommand executor = new OBCommand(this, islandManager, playerDataManager, levelManager, bossBarManager);
-        cmd.setExecutor(executor);
-        cmd.setTabCompleter(executor);
+        Bukkit.getScheduler().runTaskTimer(this, bossBarManager::updateAll, 40L, 40L);
 
         getLogger().info("OneBlock enabled.");
     }
 
     @Override
     public void onDisable() {
-        if (playerDataManager != null) playerDataManager.save();
-        if (islandManager != null) islandManager.save();
-        if (bossBarManager != null) bossBarManager.clearAll();
-    }
-
-    public PlayerDataManager getPlayerDataManager() { return playerDataManager; }
-    public IslandManager getIslandManager() { return islandManager; }
-    public LevelManager getLevelManager() { return levelManager; }
-    public BossBarManager getBossBarManager() { return bossBarManager; }
-
-    public void reloadEverything() {
-        reloadConfig();
-        levelManager.reload();
-        islandManager.reload();
-        bossBarManager.refreshAll();
-    }
-
-    private void saveResourceIfMissing(String name) {
-        if (getResource(name) != null && !new java.io.File(getDataFolder(), name).exists()) {
-            saveResource(name, false);
+        if (playerDataManager != null) {
+            playerDataManager.save();
         }
+        if (bossBarManager != null) {
+            bossBarManager.removeAll();
+        }
+    }
+
+    private void createVoidWorld() {
+        String worldName = getConfig().getString("world-name", "oneblock_world");
+        World world = Bukkit.getWorld(worldName);
+        if (world != null) return;
+
+        WorldCreator creator = new WorldCreator(worldName);
+        creator.generator(new VoidChunkGenerator());
+        creator.generateStructures(false);
+        Bukkit.createWorld(creator);
+    }
+
+    public IslandManager getIslandManager() {
+        return islandManager;
+    }
+
+    public PlayerDataManager getPlayerDataManager() {
+        return playerDataManager;
+    }
+
+    public LevelManager getLevelManager() {
+        return levelManager;
+    }
+
+    public BossBarManager getBossBarManager() {
+        return bossBarManager;
     }
 }
