85545efee7ee7b3a3ec9af1677337b9360dd9f7b
David0 <81240773+David0J@users.noreply.github.com>
2026-03-13 13:25:09 +0200

    Update IslandData.java

--- src/main/java/me/davidjawhar/oneblock/island/IslandData.java
+++ src/main/java/me/davidjawhar/oneblock/island/IslandData.java
@@ -1,39 +1,57 @@
 package me.davidjawhar.oneblock.island;
 
 import org.bukkit.Location;
-import org.bukkit.World;
 
 import java.util.UUID;
 
 public class IslandData {
-    private final UUID uuid;
+
+    private final UUID owner;
     private final int index;
-    private final String worldName;
     private final int x;
     private final int y;
     private final int z;
+    private final String worldName;
 
-    public IslandData(UUID uuid, int index, String worldName, int x, int y, int z) {
-        this.uuid = uuid;
+    public IslandData(UUID owner, int index, int x, int y, int z, String worldName) {
+        this.owner = owner;
         this.index = index;
-        this.worldName = worldName;
         this.x = x;
         this.y = y;
         this.z = z;
+        this.worldName = worldName;
     }
 
-    public UUID getUuid() { return uuid; }
-    public int getIndex() { return index; }
-    public String getWorldName() { return worldName; }
-    public int getX() { return x; }
-    public int getY() { return y; }
-    public int getZ() { return z; }
-
-    public Location getBaseLocation(World world) {
-        return new Location(world, x, y, z);
+    public UUID getOwner() {
+        return owner;
     }
 
-    public Location getTeleportLocation(World world) {
-        return new Location(world, x + 0.5, y + 1.0, z + 0.5);
+    public int getIndex() {
+        return index;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public String getWorldName() {
+        return worldName;
+    }
+
+    public boolean isOneBlock(Location location) {
+        if (location == null || location.getWorld() == null) return false;
+        if (!location.getWorld().getName().equals(worldName)) return false;
+
+        return location.getBlockX() == x
+                && location.getBlockY() == y
+                && location.getBlockZ() == z;
     }
 }
